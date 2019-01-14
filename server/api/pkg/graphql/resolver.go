bde800db76eaed727b011119f2c28de1b283c897
alex-agency <alex.agency@gmail.com>
2019-01-14 20:29:29 +0200

    generated graphql code

---
+++ server/api/pkg/graphql/resolver.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+)
+
+type Resolver struct{}
+
+func (r *Resolver) Mutation() MutationResolver {
+	return &mutationResolver{r}
+}
+func (r *Resolver) Query() QueryResolver {
+	return &queryResolver{r}
+}
+
+type mutationResolver struct{ *Resolver }
+
+func (r *mutationResolver) AddEvent(ctx context.Context, title string, info string) (*Event, error) {
+	panic("not implemented")
+}
+
+type queryResolver struct{ *Resolver }
+
+func (r *queryResolver) Events(ctx context.Context) ([]*Event, error) {
+	panic("not implemented")
+}
+func (r *queryResolver) Event(ctx context.Context, id string) (*Event, error) {
+	panic("not implemented")
+}
