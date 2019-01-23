280a282eb51aac3e63e5026e98aa585739d1fc72
Alexander Zavgorodniy <alex.agency@gmail.com>
2019-01-23 12:31:59 +0200

    move to apollo server express

--- serverless/src/server.ts
+++ serverless/src/server.ts
@@ -1,15 +1,27 @@
 import { GraphQLModule } from '@graphql-modules/core';
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express'
+import http from 'http'
 
 export async function bootstrap(AppModule: GraphQLModule) {
   const { schema, context } = AppModule;
-  const server = new ApolloServer({
+  const apollo = new ApolloServer({
     schema,
     context,
     introspection: true,
+    playground: true,
   });
 
-  server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-  });
+  const app = express()
+  apollo.applyMiddleware({ app, path: '/graphql' });
+
+  const server = http.createServer(app);
+  apollo.installSubscriptionHandlers(server)
+
+  server.listen({ port: 4000 }, () =>
+    console.log(
+      '🚀  Server ready at',
+      `http://localhost:4000${apollo.graphqlPath}`
+    )
+  )
 }
